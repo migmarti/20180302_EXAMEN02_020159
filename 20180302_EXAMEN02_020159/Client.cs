88596dc64254f6dccf7987e83821c7c6d98eb061
Unknown <mmart96@hotmail.com>
2018-04-27 21:25:00 -0700

    Commit #4

--- 20180302_EXAMEN02_020159/Client.cs
+++ 20180302_EXAMEN02_020159/Client.cs
@@ -1,6 +1,7 @@
 ﻿using _20180302_EXAMEN02_020159.Tasks;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,16 @@ namespace _20180302_EXAMEN02_020159 {
         public void doTasks(Queue<String> taskQueue) {
             int robotIndex = 0, invokerIndex = 0;
             Invoker invoker = new Invoker(taskQueue.Count);
+            try {
+                List<String> tasks = File.ReadAllLines("tasks.txt").ToList();
+                if (tasks.Count > 0) {
+                    taskQueue = new Queue<String>();
+                    foreach (String t in tasks) {
+                        taskQueue.Enqueue(t);
+                    }
+                }
+            }
+            catch (Exception e) { }
             while (taskQueue.Count > 0) {
                 Robot currentRobot = this.robots[robotIndex];
                 if (currentRobot.isFull()) {
@@ -37,9 +48,21 @@ namespace _20180302_EXAMEN02_020159 {
                     invoker.threadedInvoke(invokerIndex);
                     this.robotTaskStack.Push(robotTask);
                     invokerIndex++;
+                    saveQueue(taskQueue);
                 }
             }
             Console.WriteLine("All tasks have been assigned.");
+        }
+
+        private void saveQueue(Queue<String> taskQueue) {
+            const string sPath = "tasks.txt";
+
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sPath);
+            foreach (var item in taskQueue) {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
         }
 
         public void undoAction() {
